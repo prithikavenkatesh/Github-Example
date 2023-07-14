a2108f19f8f51d208545b9aa3a063946c7f06dda
Prithika <prithikavenkatesh@gmail.com>
2023-07-14 22:20:34 +0800

    Initial Commit

---
+++ Github Example/Github_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExampleApp.swift
+//  Github Example
+//
+//  Created by Prithika Venkatesh on 14/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
